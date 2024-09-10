29d87d46350dcbe8f5ceb57aad478e640befbdd1
Arc13x-Ian <arc13x@gmail.com>
2024-09-10 09:40:56 -0700

    camerachanges

--- CeltTactics152/Assets/Scripts/BasicCameraScript.cs
+++ CeltTactics152/Assets/Scripts/BasicCameraScript.cs
@@ -17,22 +17,32 @@ public class BasicCameraScript : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.up * Time.deltaTime * spd);
+            transform.Translate(Vector3.left * Time.deltaTime * spd);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.down * Time.deltaTime * spd);
+            transform.Translate(Vector3.right * Time.deltaTime * spd);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * spd);
+            transform.Translate(Vector3.forward * Time.deltaTime * spd);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * spd);
+            transform.Translate(Vector3.back * Time.deltaTime * spd);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * spd);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * spd);
         }
     }
 }
